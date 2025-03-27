2c4c46fa9c6824961c937a32c334abfdd862d4cd
igorpiddev <igorpiddev@gmail.com>
2025-03-27 14:50:28 +0200

    install & configure basic plugins and modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/tailwindcss', 'motion-v/nuxt', '@nuxtjs/seo', '@nuxt/image'],
+  alias: 
+    {
+      "~": "/<srcDir>",
+      "@": "/<srcDir>",
+      "~~": "/<rootDir>",
+      "@@": "/<rootDir>",
+      "#shared": "/<rootDir>/shared",
+      "assets": "/<srcDir>/assets",
+      "public": "/<srcDir>/public",
+      "#build": "/<rootDir>/.nuxt",
+      "#internal/nuxt/paths": "/<rootDir>/.nuxt/paths.mjs"
+    }
 })
